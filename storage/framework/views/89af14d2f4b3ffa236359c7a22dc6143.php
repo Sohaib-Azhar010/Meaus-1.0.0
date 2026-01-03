cc2a8c7b0a5fed28043d3faff6b90874171a7320
Sohaib Azhar <sohaibazhar04@gmail.com>
2026-01-03 15:40:18 +0500

    Update hero section background and modify text content on index page for rebranding

--- storage/framework/views/89af14d2f4b3ffa236359c7a22dc6143.php
+++ storage/framework/views/89af14d2f4b3ffa236359c7a22dc6143.php
@@ -9,11 +9,12 @@
         <div class="row justify-content-start">
             <div class="col-lg-8 text-center text-lg-start">
                 <h5 class="d-inline-block text-primary text-uppercase border-bottom border-5"
-                    style="border-color: rgba(256, 256, 256, .3) !important;">Welcome To Medinova</h5>
-                <h1 class="display-1 text-white mb-md-4">Best Healthcare Solution In Your City</h1>
+                    style="border-color: rgba(256, 256, 256, .3) !important;">Welcome To MEAUS</h5>
+                <h1 class="display-5 text-white mb-md-4">Middle East Africa Uveitis Society</h1>
+                <h1 class="display-5 text-white mb-md-4">Advancing Education, Research, and Collaboration in Uveitis and Ocular Inflammation</h1>
                 <div class="pt-2">
-                    <a href="<?php echo e(route('search')); ?>" class="btn btn-light rounded-pill py-md-3 px-md-5 mx-2">Find Doctor</a>
-                    <a href="<?php echo e(route('appointment')); ?>" class="btn btn-outline-light rounded-pill py-md-3 px-md-5 mx-2">Appointment</a>
+                    <a href="" class="btn btn-light rounded-pill py-md-3 px-md-5 mx-2">Learn More</a>
+                    <a href="" class="btn btn-outline-light rounded-pill py-md-3 px-md-5 mx-2">View Upcoming Events</a>
                 </div>
             </div>
         </div>
