95391f6f04e6a5fc4d34b26cc188ac1d765bb6d4
Sohaib Azhar <sohaibazhar04@gmail.com>
2026-01-03 16:34:41 +0500

    Revise footer layout and content to better represent MEAUS, including updated sections for About, Activities, Resources, and Contact & Legal, while enhancing the subscription form and social media links.

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -2,49 +2,11 @@
 <div class="container-fluid bg-dark text-light mt-5 py-5">
     <div class="container py-5">
         <div class="row g-5">
+            <!-- Column 1: About MEAUS -->
             <div class="col-lg-3 col-md-6">
                 <h4 class="d-inline-block text-primary text-uppercase border-bottom border-5 border-secondary mb-4">
-                    Get In Touch</h4>
-                <p class="mb-4">No dolore ipsum accusam no lorem. Invidunt sed clita kasd clita et et dolor sed
-                    dolor</p>
-                <p class="mb-2"><i class="fa fa-map-marker-alt text-primary me-3"></i>123 Street, New York, USA</p>
-                <p class="mb-2"><i class="fa fa-envelope text-primary me-3"></i>info@example.com</p>
-                <p class="mb-0"><i class="fa fa-phone-alt text-primary me-3"></i>+012 345 67890</p>
-            </div>
-            <div class="col-lg-3 col-md-6">
-                <h4 class="d-inline-block text-primary text-uppercase border-bottom border-5 border-secondary mb-4">
-                    Quick Links</h4>
-                <div class="d-flex flex-column justify-content-start">
-                    <a class="text-light mb-2" href="{{ route('home') }}"><i class="fa fa-angle-right me-2"></i>Home</a>
-                    <a class="text-light mb-2" href="{{ route('about') }}"><i class="fa fa-angle-right me-2"></i>About Us</a>
-                    <a class="text-light mb-2" href="{{ route('service') }}"><i class="fa fa-angle-right me-2"></i>Our Services</a>
-                    <a class="text-light mb-2" href="{{ route('team') }}"><i class="fa fa-angle-right me-2"></i>Meet The Team</a>
-                    <a class="text-light mb-2" href="{{ route('blog') }}"><i class="fa fa-angle-right me-2"></i>Latest Blog</a>
-                    <a class="text-light" href="{{ route('contact') }}"><i class="fa fa-angle-right me-2"></i>Contact Us</a>
-                </div>
-            </div>
-            <div class="col-lg-3 col-md-6">
-                <h4 class="d-inline-block text-primary text-uppercase border-bottom border-5 border-secondary mb-4">
-                    Popular Links</h4>
-                <div class="d-flex flex-column justify-content-start">
-                    <a class="text-light mb-2" href="{{ route('home') }}"><i class="fa fa-angle-right me-2"></i>Home</a>
-                    <a class="text-light mb-2" href="{{ route('about') }}"><i class="fa fa-angle-right me-2"></i>About Us</a>
-                    <a class="text-light mb-2" href="{{ route('service') }}"><i class="fa fa-angle-right me-2"></i>Our Services</a>
-                    <a class="text-light mb-2" href="{{ route('team') }}"><i class="fa fa-angle-right me-2"></i>Meet The Team</a>
-                    <a class="text-light mb-2" href="{{ route('blog') }}"><i class="fa fa-angle-right me-2"></i>Latest Blog</a>
-                    <a class="text-light" href="{{ route('contact') }}"><i class="fa fa-angle-right me-2"></i>Contact Us</a>
-                </div>
-            </div>
-            <div class="col-lg-3 col-md-6">
-                <h4 class="d-inline-block text-primary text-uppercase border-bottom border-5 border-secondary mb-4">
-                    Newsletter</h4>
-                <form action="">
-                    <div class="input-group">
-                        <input type="text" class="form-control p-3 border-0" placeholder="Your Email Address">
-                        <button class="btn btn-primary">Sign Up</button>
-                    </div>
-                </form>
-                <h6 class="text-primary text-uppercase mt-4 mb-3">Follow Us</h6>
+                    About MEAUS</h4>
+                <p class="mb-4">Middle East Africa Uveitis Society (MEAUS) is a non-profit independent scientific society dedicated to advancing education, research, and collaboration in uveitis and ocular inflammation across the Middle East and Africa region.</p>
                 <div class="d-flex">
                     <a class="btn btn-lg btn-primary btn-lg-square rounded-circle me-2" href="#!"><i
                             class="fab fa-twitter"></i></a>
@@ -56,6 +18,53 @@
                             class="fab fa-instagram"></i></a>
                 </div>
             </div>
+            
+            <!-- Column 2: Activities -->
+            <div class="col-lg-3 col-md-6">
+                <h4 class="d-inline-block text-primary text-uppercase border-bottom border-5 border-secondary mb-4">
+                    Activities</h4>
+                <div class="d-flex flex-column justify-content-start">
+                    <a class="text-light mb-2" href="{{ route('home') }}"><i class="fa fa-angle-right me-2"></i>Home</a>
+                    <a class="text-light mb-2" href="#"><i class="fa fa-angle-right me-2"></i>About Us</a>
+                    <a class="text-light mb-2" href="#"><i class="fa fa-angle-right me-2"></i>Leadership</a>
+                    <a class="text-light mb-2" href="#"><i class="fa fa-angle-right me-2"></i>International Speakers</a>
+                    <a class="text-light mb-2" href="#"><i class="fa fa-angle-right me-2"></i>Events & Participation</a>
+                    <a class="text-light mb-2" href="#"><i class="fa fa-angle-right me-2"></i>Achievements</a>
+                    <a class="text-light" href="#"><i class="fa fa-angle-right me-2"></i>Sponsorship</a>
+                </div>
+            </div>
+            
+            <!-- Column 3: Resources -->
+            <div class="col-lg-3 col-md-6">
+                <h4 class="d-inline-block text-primary text-uppercase border-bottom border-5 border-secondary mb-4">
+                    Resources</h4>
+                <div class="d-flex flex-column justify-content-start">
+                    <a class="text-light mb-2" href="#"><i class="fa fa-angle-right me-2"></i>History & Mission</a>
+                    <a class="text-light mb-2" href="#"><i class="fa fa-angle-right me-2"></i>Our Goals</a>
+                    <a class="text-light mb-2" href="#"><i class="fa fa-angle-right me-2"></i>Founding Members</a>
+                    <a class="text-light mb-2" href="#"><i class="fa fa-angle-right me-2"></i>Executive Board</a>
+                    <a class="text-light mb-2" href="#"><i class="fa fa-angle-right me-2"></i>Committees</a>
+                    <a class="text-light mb-2" href="#"><i class="fa fa-angle-right me-2"></i>News & Updates</a>
+                    <a class="text-light" href="#"><i class="fa fa-angle-right me-2"></i>Gallery</a>
+                </div>
+            </div>
+            
+            <!-- Column 4: Contact & Legal -->
+            <div class="col-lg-3 col-md-6">
+                <h4 class="d-inline-block text-primary text-uppercase border-bottom border-5 border-secondary mb-4">
+                    Contact & Legal</h4>
+                <p class="mb-2"><i class="fa fa-map-marker-alt text-primary me-3"></i>Middle East & Africa Region</p>
+                <p class="mb-2"><i class="fa fa-envelope text-primary me-3"></i>info@meaus.org</p>
+                <p class="mb-4"><i class="fa fa-globe text-primary me-3"></i>www.meaus.org</p>
+                <h6 class="text-primary text-uppercase mb-3">Stay Updated</h6>
+                <form action="">
+                    <div class="input-group">
+                        <input type="email" class="form-control p-3 border-0" placeholder="Your Email Address" required>
+                        <button class="btn btn-primary" type="submit">Subscribe</button>
+                    </div>
+                </form>
+                <p class="text-muted small mt-2 mb-0"><i class="fa fa-shield-alt me-2"></i>We respect your privacy</p>
+            </div>
         </div>
     </div>
 </div>
@@ -63,13 +72,15 @@
     <div class="container">
         <div class="row g-5">
             <div class="col-md-6 text-center text-md-start">
-                <p class="mb-md-0">&copy; <a class="text-primary" href="#!">Your Site Name</a>. All Rights Reserved.
+                <p class="mb-md-0">&copy; <a class="text-primary" href="{{ route('home') }}">MEAUS</a> - Middle East Africa Uveitis Society. All Rights Reserved.
                 </p>
             </div>
             <div class="col-md-6 text-center text-md-end">
-                <p class="mb-0">Designed by <a class="text-primary" href="https://htmlcodex.com"
-                        target="_blank">HTML Codex</a>. Distributed by <a href="https://themewagon.com"
-                        target="_blank">ThemeWagon</a>.</p>
+                <p class="mb-0">
+                    <a class="text-primary me-3" href="#">Privacy Policy</a>
+                    <a class="text-primary me-3" href="#">Terms of Service</a>
+                    <span class="badge bg-primary">Compliance</span>
+                </p>
             </div>
         </div>
     </div>
